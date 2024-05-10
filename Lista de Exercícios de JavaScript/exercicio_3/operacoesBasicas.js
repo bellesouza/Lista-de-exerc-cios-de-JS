f3a6f3c95f69e5d33d2e7dba96584fe1fe0f2d29
bellesouza <127145848+bellesouza@users.noreply.github.com>
2024-05-10 09:44:48 -0300

    Add files via upload

---
+++ Lista de Exercícios de JavaScript/exercicio_3/operacoesBasicas.js
@@ -0,0 +1,21 @@
+
+
+alert ("digite 2 numeros e irei apresentar todos os cálculos simples de uma calculadora, sendo elas: soma, subtração,multiplicação e divisão")
+var numero1 = Number(prompt("Digite o primeiro numero:"));
+var numero2 = Number(prompt("Digite o segundo numero:"));
+
+var soma = numero1 + numero2;
+console.log("A soma dos dois numeros é " + soma + "");
+
+var subtracao = numero1 - numero2;
+console.log("A subtração dos dois numeros é " + subtracao + "");
+
+var multiplicação = numero1 * numero2
+console.log("A multiplicação dos dois numeros é " + multiplicação + "");
+
+var divisão = numero1 / numero2
+console.log("A divisão dos dois numeros é " + divisão + "");
+
+
+
+
